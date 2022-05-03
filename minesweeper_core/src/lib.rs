3c57fd0ca5cd7e3552196018ca75aae85df083d8
Alexander Gil <pando855@gmail.com>
2022-05-03 19:09:18 +0200

    feat: Max thread limited to num cpus

--- minesweeper_core/src/lib.rs
+++ minesweeper_core/src/lib.rs
@@ -3,18 +3,22 @@ use std::fmt;
 use std::sync::mpsc;
 use std::thread;
 
+use num_integer::div_ceil;
 use rand::rngs::SmallRng;
 use rand::{Rng, RngCore, SeedableRng};
 
-fn pick_random<T>(rnd: &mut impl RngCore, v: &mut Vec<T>) -> (T, Vec<T>)
+fn pick_random<T>(rnd: &mut impl RngCore, v: &mut Vec<T>) -> Option<(T, Vec<T>)>
 where
     T: Clone,
 {
+    if v.is_empty() {
+        return None;
+    }
     let index: usize = rnd.gen_range(0..v.len());
     let random = v[index].clone();
     v[index] = v.last().unwrap().clone();
     v.pop();
-    (random, v.to_vec())
+    Some((random, v.to_vec()))
 }
 
 #[derive(Debug, Clone)]
@@ -29,10 +33,9 @@ impl fmt::Display for MinesOverflowError {
 
 impl Error for MinesOverflowError {}
 
-
 fn get_mines(mines_n: usize, len: usize) -> Result<Vec<bool>, MinesOverflowError> {
     if mines_n > len {
-        return Err(MinesOverflowError)
+        return Err(MinesOverflowError);
     }
 
     let mut mines = vec![false; len];
@@ -40,7 +43,8 @@ fn get_mines(mines_n: usize, len: usize) -> Result<Vec<bool>, MinesOverflowError
     let mut rng = SmallRng::from_entropy();
 
     for _ in 0..mines_n {
-        let (mine_index, new_indexes) = pick_random(&mut rng, &mut indexes);
+        // safe unwrap: len is already checked
+        let (mine_index, new_indexes) = pick_random(&mut rng, &mut indexes).unwrap();
         indexes = new_indexes;
         mines[mine_index] = true;
     }
@@ -58,46 +62,63 @@ pub fn get_grid(
 
     let (tx, rx) = mpsc::channel();
     let mut rng = SmallRng::from_entropy();
-    let mut workers = (0..width).collect();
+    let mut cols = (0..width).collect();
+
+    let max_threads = num_cpus::get();
+
+    let cols_per_thread = div_ceil(width, max_threads);
 
     let mut threads = vec![];
-    let mut remaining_mines = mines_n.clone();
-    // limit maximum threads to maximum CPUs available
-    // split grid in equal areas to match maximum workers
-    for i in 0..width {
-        let (x, new_workers) = pick_random(&mut rng, &mut workers);
-        workers = new_workers;
-        let worker_tx = tx.clone();
-        let worker_mines = if i != width - 1 {
-            let possible_mines = rng.gen_range(0..remaining_mines);
-            if possible_mines > height {
-                height
+    let mut remaining_mines = mines_n;
+    let mut col_counter = 0;
+
+    for _ in 0..max_threads {
+        let mut thread_work = vec![];
+        'thread_work: for _ in 0..cols_per_thread {
+            let (x, remaining_cols) = match pick_random(&mut rng, &mut cols) {
+                None => break 'thread_work,
+                Some(y) => y,
+            };
+            cols = remaining_cols;
+            let col_mines = if col_counter != width - 1 {
+                let possible_mines = rng.gen_range(0..remaining_mines);
+                if possible_mines > height {
+                    height
+                } else {
+                    possible_mines
+                }
             } else {
-                possible_mines
-            }
-        } else {
-            remaining_mines
-        };
-        remaining_mines -= worker_mines;
+                remaining_mines
+            };
+            col_counter += 1;
+            remaining_mines -= col_mines;
+            thread_work.push((x, col_mines))
+        }
+        let thread_tx = tx.clone();
 
         threads.push(thread::spawn(move || {
-            let mines = get_mines(worker_mines, height).unwrap();
-            worker_tx.send((x, mines)).unwrap();
+            let mut result = vec![];
+            for work in thread_work {
+                let mines = get_mines(work.1, height).unwrap();
+                result.push((work.0, mines))
+            }
+            thread_tx.send(result).unwrap();
         }));
     }
 
     let mut received = vec![];
-    for _ in 0..width {
+    for _ in 0..max_threads {
         received.push(rx.recv().unwrap());
     }
 
-    let mut mines = vec![];
-    for i in 0..received.len() {
-        for r in &received {
-            if i == r.0 as usize {
-                mines.push(r.1.clone());
-                break;
-            }
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    let mut mines = vec![vec![]; width];
+    for r in &received {
+        for (col_idx, col_mines) in r {
+            mines[*col_idx] = col_mines.clone();
         }
     }
     Ok(mines)
@@ -110,7 +131,7 @@ mod tests {
     #[test]
     fn test_pick_random() {
         let mut rng = SmallRng::from_entropy();
-        let (value, v) = pick_random(&mut rng, &mut vec![1, 2, 3]);
+        let (value, v) = pick_random(&mut rng, &mut vec![1, 2, 3]).unwrap();
         assert_eq!(value >= 1, true);
         assert_eq!(value <= 3, true);
         assert_eq!(v.len(), 2);
@@ -125,8 +146,8 @@ mod tests {
             let mines_vec_result = get_mines(input_mines, total_len);
             if let Ok(mines_vec) = mines_vec_result {
                 let mines_n = mines_vec
-                .into_iter()
-                .fold(0, |acc, x| if x == true { acc + 1 } else { acc });
+                    .into_iter()
+                    .fold(0, |acc, x| if x == true { acc + 1 } else { acc });
                 assert_eq!(mines_n, input_mines)
             } else {
                 assert_eq!(mines_vec_result, Err(MinesOverflowError))
@@ -144,10 +165,11 @@ mod tests {
 
             let mines_grid_res = get_grid(width, height, input_mines);
             if let Ok(mines_grid) = mines_grid_res {
-                let mines_n = mines_grid
-                    .into_iter()
-                    .flatten()
-                    .fold(0, |acc, x| if x == true { acc + 1 } else { acc });
+                let mines_n =
+                    mines_grid
+                        .into_iter()
+                        .flatten()
+                        .fold(0, |acc, x| if x == true { acc + 1 } else { acc });
                 assert_eq!(mines_n, input_mines)
             } else {
                 assert_eq!(mines_grid_res, Err(MinesOverflowError))
